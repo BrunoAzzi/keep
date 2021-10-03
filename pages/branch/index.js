28efd2fb299629532cbafd16cf0c89bd661c7a7c
Bruno Azzi <bruno.azzi@hotmail.com>
2021-10-03 08:11:46 -0300

    refactor: use next.js redirect params instead of 301 status

--- pages/branch/index.js
+++ pages/branch/index.js
@@ -5,10 +5,12 @@ export async function getServerSideProps({ req, res }) {
     const branchListResponse = await branchCollection.get();
     const branchList = await handleBranchList(branchListResponse.docs);
 
-    res.writeHead(301, {
-        Location: `/branch/${branchList[0].id}/dashboard/class`
-    });
-    res.end();
+    return {
+        redirect: {
+            destination: `/branch/${branchList[0].id}/dashboard/class`,
+            permanent: true
+        }
+    };
 }
 
 const BranchIndexPage = () => <></>;
